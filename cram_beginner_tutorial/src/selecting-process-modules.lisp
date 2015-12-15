ae858812ff9a08c3d04fae72be958bb7ecc0c23c
Lisset Y Salinas Pinacho <lisset.salinas.pinacho@gmail.com>
2015-12-15 12:43:18 +0100

    Update selecting-process-modules.lisp

--- cram_beginner_tutorial/src/selecting-process-modules.lisp
+++ cram_beginner_tutorial/src/selecting-process-modules.lisp
@@ -1,7 +1,7 @@
 (in-package :tut)
 
-(cram-reasoning:def-fact-group navigation-action-designator (action-desig)
-  (cram-reasoning:<- (action-desig ?designator (navigation ?goal))
+(cram-prolog:def-fact-group navigation-action-designator (action-desig)
+  (cram-prolog:<- (action-desig ?designator (navigation ?goal))
   (desig-prop ?designator (type navigation))
   (desig-prop ?designator (goal ?goal))))
 
@@ -15,17 +15,17 @@
           (print (cl-transforms:origin (reference action-goal)))
           (move-to (cl-transforms:origin (reference action-goal)))))))
 
-(cram-reasoning:def-fact-group turtle-navigation (cram-process-modules:matching-process-module
+(cram-prolog:def-fact-group turtle-navigation (cram-process-modules:matching-process-module
                                    cram-process-modules:available-process-module)
-  (cram-reasoning:<- (cram-process-modules:matching-process-module ?designator turtle-navigation-handler)
+  (cram-prolog:<- (cram-process-modules:matching-process-module ?designator turtle-navigation-handler)
     (desig-prop ?designator (type navigation)))
-  (cram-reasoning:<- (cram-process-modules:available-process-module turtle-navigation-handler)))
+  (cram-prolog:<- (cram-process-modules:available-process-module turtle-navigation-handler)))
 
-(cram-reasoning:def-fact-group turtle-actuators (cram-process-modules:matching-process-module
+(cram-prolog:def-fact-group turtle-actuators (cram-process-modules:matching-process-module
                                   cram-process-modules:available-process-module)
-  (cram-reasoning:<- (cram-process-modules:matching-process-module ?designator turtle-actuators)
+  (cram-prolog:<- (cram-process-modules:matching-process-module ?designator turtle-actuators)
     (desig-prop ?designator (type shape)))
-  (cram-reasoning:<- (cram-process-modules:available-process-module turtle-actuators)))
+  (cram-prolog:<- (cram-process-modules:available-process-module turtle-actuators)))
 
 (defun do-action-designator (action-desig)
   (let ((turtle-name "turtle1"))
@@ -34,4 +34,3 @@
     (top-level
       (cpm:with-process-modules-running (turtle-navigation-handler turtle-actuators)
         (cram-plan-library:perform action-desig)))))
-
